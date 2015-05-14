1a7cbc46a1893c8c4731f316bdf0bcc922179a6e
Sean-Lan <sean.lan.thu@gmail.com>
2015-05-14 20:38:42 +0800

    Add subprocess tutorial.

---
+++ subprocess/subprocess_signal.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import os
+import signal
+import subprocess
+import time
+import sys
+
+proc = subprocess.Popen(['python', 'signal_child.py'])
+print 'PARENT      : Pausing before sending signal...'
+sys.stdout.flush()
+time.sleep(1)
+print 'PARENT      : Signaling child'
+sys.stdout.flush()
+os.kill(proc.pid, signal.SIGUSR1)
